cb110f870f00b2357b385508c554615d61670d1a
SSOfy <support@ssofy.com>
2024-01-30 12:33:18 +0300

    fixed blade view message

--- scaffold/resources/views/otp/sms.blade.php
+++ scaffold/resources/views/otp/sms.blade.php
@@ -1,1 +1,1 @@
-{{ $code }} is your {{ $vars['brand'] }} verification code for {{ Str::title(str_replace('_', ' ', $option->type)) }}.
+{{ $code }} is your {{ $vars['brand'] }} verification code for {{ Str::title(str_replace('_', ' ', $option->action)) }}.
